e46896dcbf146fd136bd93fb1b53ce443ddce731
Austin Carman <abcarman12@gmail.com>
2021-11-19 13:00:55 -1000

    check prop type in questions list component

--- src/components/QuestionsList.js
+++ src/components/QuestionsList.js
@@ -1,9 +1,18 @@
-const QuestionsList = () => {
+import PropTypes from "prop-types";
+
+const QuestionsList = (props) => {
+  const { questions } = props;
+  console.log(questions);
+
   return (
     <div>
       <h2>Questions List Comp</h2>
     </div>
   );
+};
+
+QuestionsList.propTypes = {
+  questions: PropTypes.array,
 };
 
 export default QuestionsList;

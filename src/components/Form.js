e3d4879db4df9015e29bb5bd8d36b5218b3bad5e
Austin Carman <abcarman12@gmail.com>
2021-11-19 11:43:07 -1000

    added autowidth to dropdown field size

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,38 +1,51 @@
 import { useState } from "react";
-import { FormControl, Select, MenuItem } from "@mui/material";
+import {
+  FormControl,
+  Select,
+  MenuItem,
+  InputLabel,
+  FormHelperText,
+} from "@mui/material";
 
 const Form = () => {
-  const initialForm = {
-    category: "",
-    difficulty: "",
-  };
+  // const initialForm = {
+  //   category: "",
+  //   difficulty: "",
+  // };
   // eslint-disable-next-line no-unused-vars
-  const [formValues, setFormValues] = useState(initialForm);
+  const [formValues, setFormValues] = useState("");
 
   const handleChange = (e) => {
     const { value } = e.target;
-    console.log(value);
+    setFormValues(value);
+    // setFormValues({ ...formValues, [name]: value });
   };
 
   return (
-    <FormControl>
-      <Select
-        labelId="question-category-label"
-        id="question-category"
-        value={formValues.category}
-        label="Category"
-        onChange={handleChange}
-      >
-        <MenuItem value={"General Knowledge"}>General Knowledge</MenuItem>
-        <MenuItem value={"Film"}>Film</MenuItem>
-        <MenuItem value={"Music"}>Music</MenuItem>
-        <MenuItem value={"TV"}>TV</MenuItem>
-        <MenuItem value={"Science and Nature"}>Science and Nature</MenuItem>
-        <MenuItem value={"Sports"}>Sports</MenuItem>
-        <MenuItem value={"Geography"}>Geography</MenuItem>
-        <MenuItem value={"History"}>History</MenuItem>
-      </Select>
-    </FormControl>
+    <div>
+      <h2>Form</h2>
+      <FormControl sx={{ m: 1, minWidth: 120 }}>
+        <InputLabel id="demo-simple-select-label">Category</InputLabel>
+        <Select
+          labelId="question-category-label"
+          id="question-category"
+          value={formValues}
+          onChange={handleChange}
+          autoWidth
+          label="Category *"
+        >
+          <MenuItem value={"General Knowledge"}>General Knowledge</MenuItem>
+          <MenuItem value={"Film"}>Film</MenuItem>
+          <MenuItem value={"Music"}>Music</MenuItem>
+          <MenuItem value={"TV"}>TV</MenuItem>
+          <MenuItem value={"Science and Nature"}>Science and Nature</MenuItem>
+          <MenuItem value={"Sports"}>Sports</MenuItem>
+          <MenuItem value={"Geography"}>Geography</MenuItem>
+          <MenuItem value={"History"}>History</MenuItem>
+        </Select>
+        <FormHelperText>Required</FormHelperText>
+      </FormControl>
+    </div>
   );
 };
 

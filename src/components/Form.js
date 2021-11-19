a35465152805ae21a39ce81286355b4733d47f1a
Austin Carman <abcarman12@gmail.com>
2021-11-19 11:22:18 -1000

    added items to select dropdown

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,17 @@
 import { useState } from "react";
-import { FormControl, Select } from "@mui/material";
+import { FormControl, Select, MenuItem } from "@mui/material";
 
 const Form = () => {
   const initialForm = {
     category: "",
     difficulty: "",
   };
+  // eslint-disable-next-line no-unused-vars
   const [formValues, setFormValues] = useState(initialForm);
 
-  const handleChange = () => {
-    console.log("Testing");
-    setFormValues({ ...formValues, category: "testing" });
+  const handleChange = (e) => {
+    const { value } = e.target;
+    console.log(value);
   };
 
   return (
@@ -21,7 +22,16 @@ const Form = () => {
         value={formValues.category}
         label="Category"
         onChange={handleChange}
-      ></Select>
+      >
+        <MenuItem value={"General Knowledge"}>General Knowledge</MenuItem>
+        <MenuItem value={"Film"}>Film</MenuItem>
+        <MenuItem value={"Music"}>Music</MenuItem>
+        <MenuItem value={"TV"}>TV</MenuItem>
+        <MenuItem value={"Science and Nature"}>Science and Nature</MenuItem>
+        <MenuItem value={"Sports"}>Sports</MenuItem>
+        <MenuItem value={"Geography"}>Geography</MenuItem>
+        <MenuItem value={"History"}>History</MenuItem>
+      </Select>
     </FormControl>
   );
 };

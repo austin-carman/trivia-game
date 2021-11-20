2460edc03dc92668c04c67073b3d7e7e3ba9d767
Austin Carman <abcarman12@gmail.com>
2021-11-20 13:41:52 -1000

    handle open, handle close, change state for modal

--- src/components/HighScores.js
+++ src/components/HighScores.js
@@ -1,24 +1,39 @@
 import { Button, Modal, Box, Typography } from "@mui/material";
+import { useState } from "react";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 const HighScores = () => {
+  const [open, setOpen] = useState(false);
+
   const handleOpen = () => {
-    console.log("open");
+    setOpen(true);
   };
 
   const handleClose = () => {
-    console.log("close");
+    setOpen(false);
   };
 
   return (
     <div>
       <Button onClick={handleOpen}>Open modal</Button>
       <Modal
-        open={false}
+        open={open}
         onClose={handleClose}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box>
+        <Box sx={style}>
           <Typography id="modal-modal-title" variant="h6" component="h2">
             Text in a modal
           </Typography>
